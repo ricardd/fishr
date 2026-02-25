c12bd80f9d57a0ea3e56001ed857064fd19ef132
Daniel Ricard <Daniel.Ricard@dfo-mpo.gc.ca>
2026-02-25 13:44:38 -0400

    add snap testing

---
+++ tests/testthat/_snaps/biomass.md
@@ -0,0 +1,8 @@
+# biomass_index errors when input is not numeric
+
+    Code
+      biomass_index("five", 10)
+    Condition
+      Error in `cpue * area_swept`:
+      ! non-numeric argument to binary operator
+
--- tests/testthat/test-cpue.R
+++ tests/testthat/test-cpue.R
@@ -55,3 +55,9 @@ test_that("cpue errors when input is not numeric", {
     error = TRUE
   )
 })
+
+test_that("cpue warns if catch and effort lengths differ", {
+  expect_snapshot(cpue(c(100, 200, 300), c(10, 20)))
+
+  expect_no_warning(cpue(c(100, 200), c(10, 20)))
+})
