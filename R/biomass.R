76364b9d9935d49dd29266c87e60e1e01bbfe0fb
Daniel Ricard <Daniel.Ricard@dfo-mpo.gc.ca>
2026-02-25 14:14:20 -0400

    adding cpue function within biomass_index

--- R/biomass.R
+++ R/biomass.R
@@ -4,6 +4,9 @@
 #'
 #' @param cpue Numeric vector of CPUE values
 #' @param area_swept Numeric vector of area swept (e.g., km^2)
+#' @param catch catch value numeric
+#' @param effort effort value numeric
+#' @param ... arguments passed to cpue function
 #'
 #' @returns A  numeric vector of biomass index values
 #' @export
@@ -11,6 +14,16 @@
 #' @examples
 #' cod_cpue <- cpue(catch=100, effort=20)
 #' biomass_index(cod_cpue, 5)
-biomass_index <- function(cpue, area_swept){
+biomass_index <- function(
+  cpue = NULL,
+  area_swept,
+  catch = NULL,
+  effort = NULL,
+  ...
+) {
+  if (is.null(cpue) && (!is.null(catch) && !is.null(effort))) {
+    cpue <- cpue(catch, effort, ...)
+  }
+
   cpue * area_swept
 }
